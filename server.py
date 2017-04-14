bc91302e0c54e1ec731dc7b46a06502449c61276
Handyman <quickfix@tuta.io>
2017-04-14 02:37:32 -0700

    Use thread.join() instead (#219)

--- server.py
+++ server.py
@@ -71,7 +71,7 @@ def main():
 
     try:
         while threadMain.is_alive():
-            time.sleep(10)
+            thread.join(10.0)
     except (KeyboardInterrupt, IOError, OSError) as e:
         import traceback
         traceback.print_exc()
