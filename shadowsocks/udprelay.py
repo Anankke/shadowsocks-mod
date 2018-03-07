15df7297dd439d9420b18294358e447039dd7e04
AkaneAkaza <AkaneAkaza@protonmail.com>
2018-03-07 12:30:40 +0800

    increase UDP buffer

--- shadowsocks/udprelay.py
+++ shadowsocks/udprelay.py
@@ -260,6 +260,8 @@ class UDPRelay(object):
         server_socket = socket.socket(af, socktype, proto)
         server_socket.bind((self._listen_addr, self._listen_port))
         server_socket.setblocking(False)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024 * 1024)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1024 * 1024)
         self._server_socket = server_socket
         self._stat_callback = stat_callback
 
